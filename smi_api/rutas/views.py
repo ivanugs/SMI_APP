1f3a80c69191dcb4306ae1f9e7c231cc3322a46f
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2023-09-06 16:50:19 -0600

    Fixed first version of algorithm

--- smi_api/rutas/views.py
+++ smi_api/rutas/views.py
@@ -14,9 +14,13 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from . import pybingmaps
-bing = pybingmaps.Bing('Insert You API key HERE')
+from dotenv import load_dotenv
+import os
+load_dotenv()
+API_KEY = os.environ.get('BING_KEY')
+bing = pybingmaps.Bing(API_KEY)
 
-def funcionEvaluacion(df_my_location):
+def funcionEvaluacion(df_my_location, my_location, reg):
     pref_Tiempo = 0.4
     pref_Distancia = 0.15
     pref_Consultorio = 0.15
@@ -26,10 +30,10 @@ def funcionEvaluacion(df_my_location):
     df_Hospitals = read_frame(qs)
     df_Hospitals = pd.DataFrame(df_Hospitals)
     df_Hospitals.head(5)
-    df_LatitudHospitals = pd.DataFrame(df_Hospitals['LATITUD'])
-    df_LongitudHospitals = pd.DataFrame(df_Hospitals['LONGITUD'])
+    df_LatitudHospitals = pd.DataFrame(df_Hospitals['lat'])
+    df_LongitudHospitals = pd.DataFrame(df_Hospitals['long'])
     df_coordenadasHospitals = df_LatitudHospitals.join(df_LongitudHospitals)
-    nbrs = NearestNeighbors(n_neighbors=15, algorithm='auto').fit(
+    nbrs = NearestNeighbors(n_neighbors=2, algorithm='auto').fit(
         df_coordenadasHospitals)
     distances, indices = nbrs.kneighbors(df_my_location)
     df_Out = pd.DataFrame(indices)
@@ -39,21 +43,19 @@ def funcionEvaluacion(df_my_location):
        # La API añade los tiempos de traslado y distancia al dataframe
 
     for i in df_Out.index:
-            indice = df_Out.iat[i, 0]
-            df_Out.at[i, 'LATITUD'] = df_Hospitals.iat[indice, 5]
-            df_Out.at[i, 'LONGITUD'] = df_Hospitals.iat[indice, 6]
-            df_Out.at[i, 'NOMBRE_DE_LA_UNIDAD'] = df_Hospitals.iat[indice, 9]
-            df_Out.at[i, 'TOTAL_DE_CONSULTORIOS'] = df_Hospitals.iat[indice, 10]
-            df_Out.at[i, 'TOTAL_MEDICOS_GENERALES_Y_ESPECIALISTAS'] = df_Hospitals.iat[indice, 13]
-            hospital_location = (
-                df_Hospitals.iat[indice, 5], df_Hospitals.iat[indice, 6])
-            df_Out.at[i, 'Tiempo_Estimado(seg)'] = bing.travelTime(
-                my_location, hospital_location)
-            df_Out.at[i, 'Distancia_Estimada(km)'] = bing.travelDistance(
-                my_location, hospital_location)
-        # Se hace la prediccion de las calificaciones mediante el algoritmo de Bayesian Ridge Regressor
-    pred = df_Out[['TOTAL_DE_CONSULTORIOS',
-                       'TOTAL_MEDICOS_GENERALES_Y_ESPECIALISTAS']]
+        indice = df_Out.iat[i,0]
+
+        df_Out.at[i, 'lat'] = df_Hospitals.iat[indice,10]  
+        df_Out.at[i, 'long'] = df_Hospitals.iat[indice,11]  
+        df_Out.at[i, 'nombre_de_la_unidad'] = df_Hospitals.iat[indice,9]   
+        df_Out.at[i, 'total_de_consultorios'] = df_Hospitals.iat[indice,10]
+        df_Out.at[i, 'total_camas_area_hospitalizacion'] = df_Hospitals.iat[indice,13]   
+        df_Out.at[i, 'total_medicos_generales_y_especialistas'] = df_Hospitals.iat[indice,15]   
+        hospital_location = (df_Hospitals.iat[indice,10], df_Hospitals.iat[indice,11])
+        df_Out.at[i, 'Tiempo_Estimado(seg)'] = bing.travelTime(my_location, hospital_location )
+        df_Out.at[i, 'Distancia_Estimada(km)'] = bing.travelDistance(my_location, hospital_location)
+    # Se hace la prediccion de las calificaciones mediante el algoritmo de Bayesian Ridge Regressor
+    pred = df_Out[['total_de_consultorios', 'total_medicos_generales_y_especialistas']]
     df_Out["Calificacion"] = reg.predict(pred)
 
         # Haciendo el PercentRank de las variables que no son estaticas sobre el data frame ya filtrado de los nearest neighbors
@@ -95,6 +97,10 @@ class RutaViewSet(viewsets.ModelViewSet):
     pagination_class = PageNumberPagination
 
     @action(detail=False, methods=["get"])
+    def get_routes(self, request):
+        return({"Hola":"Mundo"})
+
+    @action(detail=False, methods=["get"])
     def get_route(self, request):
         # Porcentajes de Preferencia, dados por el usuario. En esta etapa fueron definidos por el equipo
         pref_Tiempo = 0.4
@@ -106,7 +112,7 @@ class RutaViewSet(viewsets.ModelViewSet):
         lng = request.GET.get("lng")
         if lng != '' and lat != '':
             longitude = float(lng)
-            latitude = float(lng)
+            latitude = float(lat)
         else:
             return Response("Invalid response")
             # Importar datos
@@ -116,41 +122,40 @@ class RutaViewSet(viewsets.ModelViewSet):
         # Se eliminó un print hospitals
         df_Hospitals = pd.DataFrame(df_Hospitals)
         df_Hospitals.head(5)
-        print(df_Hospitals.head(5))
-        return Response("LLegué a los hospitales")
     
         # Juntando las coordenadas de los hospitales en un solo dataframe que es utilizado por el algoritmo de Nearest Neighbors
-        df_LatitudHospitals = pd.DataFrame(df_Hospitals['LATITUD'])
-        df_LongitudHospitals = pd.DataFrame(df_Hospitals['LONGITUD'])
+        df_LatitudHospitals = pd.DataFrame(df_Hospitals['lat'])
+        df_LongitudHospitals = pd.DataFrame(df_Hospitals['long'])
         df_coordenadasHospitals = df_LatitudHospitals.join(df_LongitudHospitals)
 
         Ev_hospitales = df_Hospitals
+        
 
         # Percentrank(Evaluacion) de los parametros Total de Consultorios y Medicos Generales (Variables Estaticas)
 
-        Ev_hospitales['Ev_Consultorios'] = Ev_hospitales.reset_index()[['TOTAL_DE_CONSULTORIOS']] \
+        Ev_hospitales['Ev_Consultorios'] = Ev_hospitales.reset_index()[['total_de_consultorios']] \
             .apply(lambda x: (x.rank(method='dense') - 1) / (x.nunique() - 1)) \
             .values
 
-        Ev_hospitales['Ev_Medicos'] = Ev_hospitales.reset_index()[['TOTAL_MEDICOS_GENERALES_Y_ESPECIALISTAS']] \
+        Ev_hospitales['Ev_Medicos'] = Ev_hospitales.reset_index()[['total_medicos_generales_y_especialistas']] \
             .apply(lambda x: (x.rank(method='dense') - 1) / (x.nunique() - 1)) \
             .values
-
+        
         # Se añade Calificacion al dataframe de evaluación la cual suma de estos dos parametros multiplicado por
         # su factor de preferencia
         Ev_hospitales["Calificacion"] = ((Ev_hospitales['Ev_Consultorios'] * pref_Consultorio)
                                          + (Ev_hospitales['Ev_Medicos'] * pref_Medicos))
-
+        
         # Solamente se seleccionaran los 10 neighbors mas cercanos
         nbrs = NearestNeighbors(n_neighbors=15, algorithm='auto').fit(
             df_coordenadasHospitals)
 
-        X = Ev_hospitales[['TOTAL_DE_CONSULTORIOS',
-                           'TOTAL_MEDICOS_GENERALES_Y_ESPECIALISTAS']]
+        X = Ev_hospitales[['total_de_consultorios',
+                           'total_medicos_generales_y_especialistas']]
         y = Ev_hospitales['Calificacion']
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.33, random_state=417)
-
+        
         regr = linear_model.BayesianRidge()
         reg = regr.fit(X_train, y_train)
         y_pred = reg.predict(X_test)
@@ -158,9 +163,9 @@ class RutaViewSet(viewsets.ModelViewSet):
         my_latitude = latitude
         my_longitude = longitude
         my_location = (my_latitude, my_longitude)
-        df_my_location = pd.DataFrame({'Latitud': [my_latitude],'Longitud': [my_longitude]})
-        df_Output = funcionEvaluacion(df_my_location)
-
+        df_my_location = pd.DataFrame({'lat': [my_latitude],'long': [my_longitude]})
+        df_Output = funcionEvaluacion(df_my_location, my_location, reg)
+        
         hcf = df_Output.sort_values(["Ranking"], ascending=True)
         hcf = hcf.head(3)
         print(hcf)
