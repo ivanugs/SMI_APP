a14cf6645e251e604ef522e2ff7f25d834543a80
ivanugs <ivansegundo702@gmail.com>
2023-09-06 17:51:02 -0600

    Add api card reader with get and post

--- smi_ino/app.js
+++ smi_ino/app.js
@@ -1,26 +1,51 @@
-const express = require("express");
+const http = require('http');
+const fs = require('fs');
+const SerialPort = require('serialport');
+const express = require('express');
+const socketIO = require('socket.io');
+const axios = require('axios');
+
 const app = express();
-const port = 3000;
-var requests = require('requests');
-var url = "http://192.168.1.70:8000/api/rutas/get_route";
+const server = http.createServer(app);
+const io = socketIO(server);
 
-function send_request (url){
-    requests(url)
-    .on("data", function (chunk) {
-      console.log(chunk);
-    })
-    .on("end", function (err) {
-      if (err) return console.log("connection closed due to errors", err);
-
-      console.log("end");
-    });
-}
-
-app.get("/", (req, res) => {
-  send_request(url)
-  res.send("Hello World!");
+const port = new SerialPort('COM8', {
+  baudRate: 115200,
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1,
+  flowControl: false
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const parser = port.pipe(new SerialPort.parsers.Readline({ delimiter: '\r\n' }));
+
+const index = fs.readFileSync('index.html', 'utf8');
+const apiUrl = 'http://192.168.1.70:8000/api/rutas/get_routes';
+
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(index);
+});
+
+io.on('connection', (socket) => {
+  console.log('Node is listening to port');
+});
+
+parser.on('data', (data) => {
+  console.log('Received data from port: ' + data);
+  io.emit('data', data);
+  sendCard(apiUrl, data);
+});
+
+async function sendCard(url, mac) {
+  try {
+    const response = await axios.get(url, { params: { mac } });
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
