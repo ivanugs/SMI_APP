2841ec813754231d8043e31bc43cbb8f1d90601c
ivanugs <ivansegundo702@gmail.com>
2023-09-07 12:04:10 -0600

    Add patient register page

--- smi_app/src/admin/index.js
+++ smi_app/src/admin/index.js
@@ -5,5 +5,7 @@ export * from './components/ListUserContent';
 
 export * from './pacientes/pages/PacientePage';
 export * from './pacientes/pages/AsistenciaPage';
+export * from './pacientes/pages/RegistroPacientePage';
 export * from './pacientes/components/PacienteComponent';
 export * from './pacientes/components/AsistenciaComponent';
+export * from './pacientes/components/RegistroPacienteComponent';
