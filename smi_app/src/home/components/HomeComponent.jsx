4aea273396b5fff9e02b99616305c93c1b3ccc44
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2023-09-07 02:56:09 -0600

    Added username to home

--- smi_app/src/home/components/HomeComponent.jsx
+++ smi_app/src/home/components/HomeComponent.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { NavBar } from '../../ui/components/NavBar'
+import React from 'react';
+import { NavBar } from '../../ui/components/NavBar';
 
 export const HomeComponent = () => {
+  const userName = localStorage.getItem("name");
   return (
     <div style={{ fontSize: '30px' }}>
-      <b>Bienvenido {}</b>
+      <b>Bienvenido {userName}</b>
 </div>
 
   );
