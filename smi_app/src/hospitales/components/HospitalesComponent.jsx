dad5b0869e72f6e1c60391fa7e48ea710416bea4
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2023-09-06 22:12:37 -0600

    Added some styles for padding separation

--- smi_app/src/hospitales/components/HospitalesComponent.jsx
+++ smi_app/src/hospitales/components/HospitalesComponent.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Card } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { getHospitals } from "../../store/slices";
 import "./HospitalComponent.css";
@@ -24,53 +24,55 @@ export const HospitalesComponent = () => {
   }, [hospitales, isLoading]);
 
   return (
-    <>
-      <Row>
-        <Col xs={10}>
-          <MapContainer
-            center={position}
-            zoom={13}
-            style={{ width: "100%", height: "calc(98vh - 4rem)" }}
-          >
-            <TileLayer
-              url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            {results.length > 0 && (
-              <>
-                {results.map((result, index) => (
-                  <Marker position={[result.lat, result.long]}>
-                    <Popup>
-                      A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                  </Marker>
-                ))}
-              </>
-            )}
-          </MapContainer>
-        </Col>
-        <Col xs={2}>
-          <h2 className="title">Hospitales Disponibles</h2>
-          <div className="custom-listgroup overflow-auto">
-            {results.length > 0 && (
-              <Row className="mt-5">
-                <Col xs={12} className="text-center">
-                  <div className="result-container">
-                    {results.map((result, index) => (
-                      <BasicHospitalResultCard
-                        key={result.id}
-                        index={index}
-                        objeto={result}
-                        active={result.active}
-                      />
-                    ))}
-                  </div>
-                </Col>
-              </Row>
-            )}
-          </div>
-        </Col>
-      </Row>
-    </>
+    <Card className="content-card" >
+      <Card.Body >
+        <Row>
+          <Col xs={10}>
+            <MapContainer
+              center={position}
+              zoom={13}
+              style={{ width: "100%", height: "calc(85vh - 4rem)" }}
+            >
+              <TileLayer
+                url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              />
+              {results.length > 0 && (
+                <>
+                  {results.map((result, index) => (
+                    <Marker position={[result.lat, result.long]}>
+                      <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                      </Popup>
+                    </Marker>
+                  ))}
+                </>
+              )}
+            </MapContainer>
+          </Col>
+          <Col xs={2}>
+            <h2 className="title">Hospitales Disponibles</h2>
+            <div className="custom-listgroup overflow-auto">
+              {results.length > 0 && (
+                <Row className="mt-5">
+                  <Col xs={12} className="text-center">
+                    <div className="result-container">
+                      {results.map((result, index) => (
+                        <BasicHospitalResultCard
+                          key={result.id}
+                          index={index}
+                          objeto={result}
+                          active={result.active}
+                        />
+                      ))}
+                    </div>
+                  </Col>
+                </Row>
+              )}
+            </div>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
   );
 };
