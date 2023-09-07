95e524a8c0634a149714bfdb13d4958571e1296d
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2023-09-07 11:56:13 -0600

    Added attendance backend logic

--- smi_api/registros/views.py
+++ smi_api/registros/views.py
@@ -7,9 +7,13 @@ from rest_framework.decorators import action
 from django.utils import timezone
 from rest_framework.response import Response
 
+import os
 from dotenv import load_dotenv
 load_dotenv()
+import requests
+import datetime
 
+SMI_ARDUINO_PATH = os.getenv('SMI_ARDUINO_PATH', default="")
 
 class RegistroViewSet(viewsets.ModelViewSet):
 
@@ -20,9 +24,7 @@ class RegistroViewSet(viewsets.ModelViewSet):
     
     @action(detail=False, methods=["get"])
     def send_register(self, request):
-        import datetime
         mac = request.GET.get("mac")
-        print(mac)
         five_minutes_ago = timezone.now() + datetime.timedelta(minutes=-5)
         existe_registro = Registro.objects.filter(card=mac, created_at__gte=five_minutes_ago)
         if not existe_registro:
@@ -32,13 +34,29 @@ class RegistroViewSet(viewsets.ModelViewSet):
         else:
             return Response({"Existe registro":mac})
         
-    @action(detail=False, methods=["get"])
-    def register_card(self, request):
-        patient_uuid = request.GET.get("patient_uuid")
-        existe_paciente = Paciente.objects.filter(uuid=patient_uuid).first()
-        if not existe_paciente.tiene_tarjeta:
-            print("No tiene tarjeta")
-            return Response({"No Existe Paciente":patient_uuid})
-        else:
-            return Response({"Existe Paciente":patient_uuid})
+    @action(detail=False, methods=["post"])
+    def register_attendance(self, request):
+        try:
+            response = requests.get(f"{SMI_ARDUINO_PATH}/data",timeout=30)
+            json_res = response.json()
+            if response.status_code == 200:
+                mac_tarjeta = json_res['data']
+                paciente = Paciente.objects.filter(mac_tarjeta=mac_tarjeta).first()
+                if paciente:
+                    five_minutes_ago = timezone.now() + datetime.timedelta(minutes=-5)
+                    existe_registro = Registro.objects.filter(card=mac_tarjeta, created_at__gte=five_minutes_ago)
+                    if not existe_registro:
+                        tipo_registro = TipoRegistro.objects.get(nombre="lectura_paciente")
+                        registro = Registro.objects.create(card=mac_tarjeta, tipo_registro=tipo_registro)
+                        return Response({"message":"Registro Exitoso", "data": {
+                            "paciente": f"{paciente.apellido_paterno} {paciente.apellido_materno} {paciente.nombres}",
+                            "tarjeta": paciente.mac_tarjeta,
+                            "hora": registro.created_at
+                        }})
+                else:
+                    return Response({"No hay Tarjeta registrada": mac_tarjeta})
+        except requests.exceptions.ReadTimeout: 
+            pass
+        
+        return Response({"Error en el registro de": mac_tarjeta})
 
--- smi_app/src/admin/pacientes/components/AsistenciaComponent.jsx
+++ smi_app/src/admin/pacientes/components/AsistenciaComponent.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
+import "./AsistenciaComponent.css";
+import { Spinner } from "../../../ui/components/Spinner";
 export const AsistenciaComponent = () => {
   return (
-    <div>AsistenciaComponent</div>
-  )
-}
+    <Row className="justify-content-center align-items-center text-center">
+      <Col xs={6}>
+        <Card className="attendance-card">
+          <Card.Header>Registro de usuario</Card.Header>
+          <Card.Body>
+            <h2>Ingresa tu tarjeta al lector</h2>
+            <Spinner />
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
