674acf5cb21b5fcc2d53bbe272f034d0abde381b
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2023-09-06 23:50:24 -0600

    Added listing for patients

--- smi_app/src/admin/pacientes/components/PacienteComponent.jsx
+++ smi_app/src/admin/pacientes/components/PacienteComponent.jsx
@@ -24,11 +24,38 @@ export const PacienteComponent = () => {
     <Card className="content-card" >
       <Card.Body >
         <Row>
-          <Col xs={10}>
-            
-          </Col>
-          <Col xs={2}>
-            
+          <Col xs={12}>
+            <h1>Lista de Pacientes</h1>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nombre</th>
+                  <th>CURP</th>
+                  <th>NSS</th>
+                  <th>IMSS</th>
+                  <th>ISSSTE</th>
+                  <th>Militar</th>
+                </tr>
+              </thead>
+              <tbody>
+              {results.length > 0 && (
+                  <>
+                  {results.map((result, index) => (
+                        <tr>
+                          <td>{result.id}</td>
+                          <td>{result.nombres}</td>
+                          <td>{result.curp}</td>
+                          <td>{result.nss}</td>
+                          <td>{result.a_imss}</td>
+                          <td>{result.a_issste}</td>
+                          <td>{result.a_militar}</td>
+                        </tr>
+                      ))}
+                  </>
+              )}
+              </tbody>
+            </table>
           </Col>
         </Row>
       </Card.Body>
